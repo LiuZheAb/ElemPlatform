281737e15a5112200e9ccd05de0c0be5b763457d
liuzhe <liu748951@163.com>
2020-07-21 18:02:56 +0800

    修改首页及文档样式

--- src/pages/Help/index.js
+++ src/pages/Help/index.js
@@ -3,6 +3,7 @@ import React, { Component, lazy } from 'react'
 import { Route } from "react-router-dom";
 import Header from "../../components/Header";
 import "./index.less";
+import background from "../../assets/images/background.png";
 
 const Viz = lazy(() => import('./viz'));
 const Dataflow = lazy(() => import('./dataflow'));
@@ -10,7 +11,7 @@ const Dataflow = lazy(() => import('./dataflow'));
 export default class index extends Component {
     render() {
         return (
-            <div>
+            <div style={{ background: `url(${background}) no-repeat center`, paddingTop: "50px" }}>
                 <Header></Header>
                 <Route exact path="/help/helpv" component={Viz} />
                 <Route exact path="/help/helpd" component={Dataflow} />
