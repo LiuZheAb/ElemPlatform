cb130b208486cf4b7156e4da7b1a8849fa554198
liuzhe <liu748951@163.com>
2020-07-21 17:16:39 +0800

    流程化嵌入页面添加头部导航栏

--- src/pages/ElemDataFlow/pages/Home/index.js
+++ src/pages/ElemDataFlow/pages/Home/index.js
@@ -1,11 +1,13 @@
 // 流程化嵌入页面
 import React, { Component } from 'react';
+import Header from "../../../../components/Header";
 
 export default class index extends Component {
     render() {
         return (
-            <div style={{ width: "100vw", height: "100vh", border: "none", overflow: "hidden" }}>
-                <iframe src="http://192.168.2.5:3000" frameBorder="0" title="dataflow" style={{ width: "100%", height: "100%" }}></iframe>
+            <div style={{ width: "100vw", height: "100vh", border: "none" }}>
+                <Header></Header>
+                <iframe src="http://192.168.2.5:3000" frameBorder="0" title="dataflow" style={{ width: "100%", height: "calc(100% - 50px)", marginTop: 50 }}></iframe>
             </div>
         )
     }
