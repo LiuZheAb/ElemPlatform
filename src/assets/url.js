a7733cbf1042ad5b351cf82b619581feb3f25c13
liuzhe <liu748951@163.com>
2020-07-28 13:42:04 +0800

    将数据流程迁入地址提取到url.js文件中

--- src/assets/url.js
+++ src/assets/url.js
@@ -4,6 +4,8 @@ export let baseUrl = "http://192.168.2.134:8002";
 export let vizUrl = "http://192.168.2.134:4000";
 // echarts可视化地址
 export let echartsUrl = "http://192.168.2.134:3300";
+// 数据流程地址
+export let dataflowUrl="http://192.168.2.5"
 
 // export let baseUrl = "http://47.92.88.112:8002";
 // export let vizUrl = "http://vtkclient.yuanjisuan.cn/vtk/";
--- src/pages/ElemDataFlow/pages/Home/index.js
+++ src/pages/ElemDataFlow/pages/Home/index.js
@@ -1,13 +1,14 @@
 // 流程化嵌入页面
 import React, { Component } from 'react';
 import Header from "../../../../components/Header";
+import { dataflowUrl } from "../../../../assets/url";
 
 export default class index extends Component {
     render() {
         return (
             <div style={{ width: "100vw", height: "100vh", border: "none" }}>
                 <Header></Header>
-                <iframe src="http://192.168.2.5" frameBorder="0" title="dataflow" style={{ width: "100%", height: "calc(100% - 50px)", marginTop: 50 }}></iframe>
+                <iframe src={dataflowUrl} frameBorder="0" title="dataflow" style={{ width: "100%", height: "calc(100% - 50px)", marginTop: 50 }}></iframe>
             </div>
         )
     }
